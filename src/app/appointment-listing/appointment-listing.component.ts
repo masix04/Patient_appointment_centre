938ba4ecb5f761450ca10f35f5cee30596e202da
muhammad.ahmed <muhammad.ahmed@sixlogics.com>
2022-04-20 11:48:30 +0500

    Appointment-listing Page Design Updated & improved Listing-tab Page Design Improved

--- src/app/appointment-listing/appointment-listing.component.ts
+++ src/app/appointment-listing/appointment-listing.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentListingComponent implements OnInit {
   CheckUpTypes: any;
-
+  gender:any;
   constructor() {
     this.CheckUpTypes = ['Bruises', 'Splean Operation', 'Kidney Transplant', 'Kidney Removal', 'ENT', 'Brain Surgery'];
+    /**  this.gender = 'Not Specified';
+     * this.gender = 'Female';
+     */
+    this.gender = 'Male';
   }
 
   ngOnInit(): void {
